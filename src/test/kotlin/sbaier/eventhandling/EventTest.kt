28512ce5fefa89a0f049d1bcb4896cd78c4a7b91
Sebastian Baier HomePC <sebastian.baier93@hotmail.de>
2019-11-25 13:30:58 +0100

    Commit addition

--- src/test/kotlin/sbaier/eventhandling/EventTest.kt
+++ src/test/kotlin/sbaier/eventhandling/EventTest.kt
@@ -1,4 +1,85 @@
 package sbaier.eventhandling
 
-class EventTest {
+import java.lang.IllegalArgumentException
+import kotlin.test.*
+
+class EventTest
+{
+    lateinit var event: Event<String>
+    lateinit var defaultListener: (String) -> Unit
+
+    @BeforeTest
+    fun setup()
+    {
+        event = Event()
+        defaultListener = { name: String -> println("The name is $name")}
+    }
+
+
+    @Test
+    fun isListening_returnsCorrectValue()
+    {
+        assertFalse(event.isListening(defaultListener))
+        event += defaultListener
+        assertTrue(event.isListening(defaultListener))
+    }
+
+    @Test
+    fun plusAssign_listenerAddedToEvent()
+    {
+        event += defaultListener
+        assertTrue(event.isListening(defaultListener))
+    }
+
+    @Test
+    fun plusAssign_DoubleAddOfSameListenerThrowsException()
+    {
+        event += defaultListener
+        assertFailsWith<IllegalArgumentException> { event += defaultListener }
+    }
+
+    @Test
+    fun minusAssign_listenerRemovedFromEvent()
+    {
+        event += defaultListener
+        assertTrue(event.isListening(defaultListener))
+        event -= defaultListener
+        assertFalse(event.isListening(defaultListener))
+    }
+
+    @Test
+    fun minusAssign_RemovingNonAddedListenerThrowsException()
+    {
+        assertFailsWith<IllegalArgumentException> { event -= defaultListener }
+    }
+
+    @Test
+    fun invoke_CallsAllListeners()
+    {
+        var counter = 0
+        val argument = "Sam"
+        val listeners: List<(String) -> Unit> = listOf (
+                {_: String -> counter += 1 },
+                {_: String -> counter += 1 },
+                {_: String -> counter += 1 })
+        for(listener in listeners)
+            event += listener
+        event(argument)
+        assertEquals(counter, event.listenerCount)
+    }
+
+    @Test
+    fun invoke_ArgumentsIdenticalForAllListeners()
+    {
+        val argument = "Sam"
+        var isSameName = true
+        val listeners: List<(String) -> Unit> = listOf (
+                {name: String -> isSameName = isSameName && name == argument},
+                {name: String -> isSameName = isSameName && name == argument},
+                {name: String -> isSameName = isSameName && name == argument})
+        for(listener in listeners)
+            event += listener
+        event(argument)
+        assertTrue(isSameName)
+    }
 }
