3e77ad3ba3a7e975efa2262b11776343d4abd5c0
BlackLambert <57714553+BlackLambert@users.noreply.github.com>
2019-12-05 18:38:36 +0100

    Update AppTest.kt
    Removes failing test.

--- src/test/kotlin/sbaier/datanet/AppTest.kt
+++ src/test/kotlin/sbaier/datanet/AppTest.kt
@@ -5,16 +5,10 @@ package sbaier.datanet
 
 import kotlin.test.Test
 import kotlin.test.assertNotNull
-import kotlin.test.assertNull
 
 class AppTest {
     @Test fun testAppHasAGreeting() {
         val classUnderTest = App()
         assertNotNull(classUnderTest.greeting, "app should have a greeting")
-    }
-    
-    @Test fun testAppNoGreeting() {
-        val classUnderTest = App()
-        assertNull(classUnderTest.greeting, "app should have a greeting")
     }
 }
